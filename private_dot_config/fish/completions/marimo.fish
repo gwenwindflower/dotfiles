aa618523882e790c385a3a1a342f280c7a63a154
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-03-31 16:08:57 -0500

    fix(fish): update marimo's completion

--- private_dot_config/fish/completions/marimo.fish
+++ private_dot_config/fish/completions/marimo.fish
@@ -1,14 +1,1 @@
-function _marimo_completion
-    set -l response (env _MARIMO_COMPLETE=fish_complete COMP_WORDS=(commandline -cp) COMP_CWORD=(commandline -t) marimo)
-    for completion in $response
-        set -l metadata (string split "," $completion)
-        if test $metadata[1] = dir
-            __fish_complete_directories $metadata[2]
-        else if test $metadata[1] = file
-            __fish_complete_path $metadata[2]
-        else if test $metadata[1] = plain
-            echo $metadata[2]
-        end
-    end
-end
-complete --no-files --command marimo --arguments "(_marimo_completion)"
+_MARIMO_COMPLETE=fish_source marimo | source
