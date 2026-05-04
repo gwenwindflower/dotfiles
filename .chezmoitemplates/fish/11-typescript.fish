2d40ba62ac619d1ef6d94c4dbec82beaa2a32919
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-05-04 13:05:22 -0500

    fix(fish): correct pnpm PATH to bin dir

--- .chezmoitemplates/fish/11-typescript.fish
+++ .chezmoitemplates/fish/11-typescript.fish
@@ -5,7 +5,7 @@ set -gx BUN_INSTALL $HOME/.bun
 fish_add_path $BUN_INSTALL/bin
 
 set -gx PNPM_HOME $XDG_DATA_HOME/pnpm
-fish_add_path $PNPM_HOME
+fish_add_path $PNPM_HOME/bin
 
 set -gx DENO_HOME $XDG_DATA_HOME/deno
 set -gx DENO_INSTALL_ROOT $DENO_HOME/bin
