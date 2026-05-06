a42f881fa41fb01e8b259f0680bc055b943aab22
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-05-06 00:04:23 -0500

    chore(fish): simplify typescript runtime path configs
    Revert to using the varying default root paths for deno, bun, and pnpm,
    instead of forcing all into the same path patterns for cache and global
    store

--- .chezmoitemplates/fish/11-typescript.fish
+++ .chezmoitemplates/fish/11-typescript.fish
@@ -1,12 +1,20 @@
 # =============================================================================
 # 11 — TypeScript (Bun, pnpm, Deno)
 # =============================================================================
-set -gx BUN_INSTALL $HOME/.bun
-fish_add_path $BUN_INSTALL/bin
 
+# These are intentionally not special env vars
+# and do not configure the tools
+# these are set to the default locations for each
+# to ensure the smoothest operation across upgrades
+# at the sacrifice of unified global store paths
+# bun and deno use the ~/.<tool> pattern like npm
+# whereas pnpm more correctly uses $XDG_DATA_HOME
+# I used to force them all into $XDG_DATA_HOME
+# but it's not worth the trouble
+set -gx BUN_HOME $HOME/.bun
+set -gx DENO_HOME $HOME/.deno
 set -gx PNPM_HOME $XDG_DATA_HOME/pnpm
-fish_add_path $PNPM_HOME/bin
 
-set -gx DENO_HOME $XDG_DATA_HOME/deno
-set -gx DENO_INSTALL_ROOT $DENO_HOME/bin
-fish_add_path $DENO_INSTALL_ROOT
+fish_add_path $BUN_HOME/bin
+fish_add_path $DENO_HOME/bin
+fish_add_path $PNPM_HOME/bin
