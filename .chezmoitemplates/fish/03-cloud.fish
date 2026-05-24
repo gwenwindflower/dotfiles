20248838dd081b43a06967b361ffcaf9241683a1
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-05-24 15:34:05 -0500

    chore(fish): move ai fragment to general cloud fragment

--- .chezmoitemplates/fish/03-cloud.fish
+++ .chezmoitemplates/fish/03-cloud.fish
@@ -1,5 +1,11 @@
 # =============================================================================
-# 03 — AI
+# 03 — Cloud
 # =============================================================================
+
+## Platforms
+set -gx GCLOUD_HOME $HOMEBREW_PREFIX/share/google-cloud-sdk
+fish_add_path $GCLOUD_HOME/bin
+
+## AI
 set -gx CLAUDE_HOME $HOME/.claude
 set -gx OPENCODE_ENABLE_EXA 1
