816e36ecad447a8264795523105f5d75a12d7ad8
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-06-27 19:50:40 -0500

    chore(fish): add indi completions

---
+++ private_dot_config/fish/exact_completions/indi.fish
@@ -0,0 +1,1 @@
+complete --command indi --no-files --arguments "(env _INDI_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) indi)" --condition "env _INDI_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) indi"
