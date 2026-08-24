6576384bb5950c64aad725137bd09e273df1b740
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-08-24 00:21:33 -0500

    chore(nvim): move d2 config to new lang_data file

--- private_dot_config/nvim/lua/plugins/lang_markdown.lua
+++ private_dot_config/nvim/lua/plugins/lang_markdown.lua
@@ -175,16 +175,4 @@ return {
       return opts
     end,
   },
-  {
-    "d2lang/d2-vim",
-    ft = { "d2" },
-    dependencies = {
-      {
-        "ravsii/tree-sitter-d2",
-        dependencies = { "nvim-treesitter/nvim-treesitter" },
-        version = "*",
-        build = "make nvim-install",
-      },
-    },
-  },
 }
