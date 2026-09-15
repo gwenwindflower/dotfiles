9f373b73ab87e5605a43dc391197a0f3f14e2679
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-09-15 09:40:37 -0500

    chore(herdr): polish worktrunk and nvim cmd bindings
    Make more consistent across fish, kitty, and herdr; herdr bindings can
    shadow fish as it will intercept them in tabs it controls and the same
    binding can flow through to fish in non-herdr panes.

--- .chezmoitemplates/fish/23-bindings.fish
+++ .chezmoitemplates/fish/23-bindings.fish
@@ -30,14 +30,10 @@ bind --user -M insert ctrl-super-g "commandline -r 'git forgit log'; commandline
 bind --user ctrl-super-g "commandline -r 'git forgit log'; commandline -f execute"
 bind --user -M insert ctrl-alt-g "commandline -r 'git forgit log'; commandline -f execute"
 bind --user ctrl-alt-g "commandline -r 'git forgit log'; commandline -f execute"
-bind --user -M insert super-G "commandline -r 'gh dash'; commandline -f execute"
-bind --user super-G "commandline -r 'gh dash'; commandline -f execute"
-bind --user -M insert alt-G "commandline -r 'gh dash'; commandline -f execute"
-bind --user alt-G "commandline -r 'gh dash'; commandline -f execute"
-bind --user -M insert ctrl-super-w "commandline -r 'wt switch'; commandline -f execute"
-bind --user ctrl-super-w "commandline -r 'wt switch'; commandline -f execute"
-bind --user -M insert ctrl-alt-w "commandline -r 'wt switch'; commandline -f execute"
-bind --user ctrl-alt-w "commandline -r 'wt switch'; commandline -f execute"
+bind --user -M insert shift-super-g "commandline -r 'wt switch'; commandline -f execute"
+bind --user shift-super-g "commandline -r 'wt switch'; commandline -f execute"
+bind --user -M insert shift-alt-g "commandline -r 'wt switch'; commandline -f execute"
+bind --user shift-alt-g "commandline -r 'wt switch'; commandline -f execute"
 
 # clearing and reloading
 bind --user -M insert super-r "fresh -r"
